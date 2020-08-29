0ec38246e8d0a80afb7b95667abfe4b497a68985
Swati Jain <swati.jain@nyu.edu>
2020-08-29 10:45:42 -0400

    Added example run for RAG-IF

--- run_min_mut_analysis.sh
+++ run_min_mut_analysis.sh
@@ -8,7 +8,7 @@ for i in Top*;do
 cd  $i
 if [  -f *.survivors ]; then
     echo "in folder:" $i
-    python /Users/sj78/Documents/labwork/MutationsForDesign/RAG-IF_Code/end_seq_order_Swati.py $1  > min_mut.analysis4_Swati   
+    python /ts_home/sj78/labwork/MutationsForDesign/RAG-IF_Code/end_seq_order.py $1  > min_mut.analysis   
 fi
 cd ..	
 
